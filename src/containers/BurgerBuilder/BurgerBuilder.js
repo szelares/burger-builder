ad4de42e982a02d7e5abbc47d1275de581cfc284
Piotr <szelares@gmail.com>
2019-06-26 22:26:47 +0200

    scripts ejected and added burger css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../setup-hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuider extends React.Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
     );
