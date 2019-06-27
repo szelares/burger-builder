59ae32694cd4a5049b718d729dedb844addb295f
Piotr <szelares@gmail.com>
2019-06-27 23:26:55 +0200

    added ingredients output in burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from "../../setup-hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuider extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 2,
+      bacon: 1,
+      meat: 1
+    }
+  };
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
